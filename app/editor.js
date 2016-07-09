b59d2ab5a9a54c84a8d3eeb235735bdf3f9fa515
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-07-09 14:35:51 -0700

    Update API according to https://github.com/uwdata/compassql/pull/124
    cc: @rileychang, @zeningqu, @felixcodes

--- app/editor.js
+++ app/editor.js
@@ -491,7 +491,7 @@ ved.cql.renderItems = function(sel, group, indexPrefix) {
 
 ved.cql.generate = function(query) {
   var startTime = Date.now();
-  var rootGroup = cql.query(query, ved.cql.schema);
+  var rootGroup = cql.query(query, ved.cql.schema).result;
   var endTime = Date.now();
   console.log('Query time:', (endTime - startTime), 'milliseconds');
 
