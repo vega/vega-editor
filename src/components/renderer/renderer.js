b1af9d77e92c709a3681fc790ec9ab79769b33c8
starry <starrynight0225@gmail.com>
2017-06-27 23:40:47 -0700

    use imported css

--- src/components/renderer/renderer.js
+++ src/components/renderer/renderer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as vega from 'vega';
+import 'vega-tooltip/build/vega-tooltip.css';
 import './index.css';
 import Vega from '../../constants'
 import * as vegaTooltip from 'vega-tooltip';
